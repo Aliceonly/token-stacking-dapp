f4ac5546ac42de2685d06eff802f14177250ab18
Aliceonly <aliceonly@foxmail.com>
2023-10-15 23:53:35 +0800

    TokenStakingLogic

--- web3/contracts/TokenStaking.sol
+++ web3/contracts/TokenStaking.sol
@@ -1,2 +1,104 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
 pragma solidity ^0.8.9;
+
+import "./Ownable.sol";
+import "./ReentrancyGuard.sol";
+import "./Initializable.sol";
+import "./IERC20.sol";
+
+contract TokenStaking is Ownable, ReentrancyGuard, Initializable {
+    //存user detail的结构体
+    struct User {
+        uint256 stakeAmount; //质押的数量
+        uint256 rewardAmount; //奖励的数量
+        uint256 lastStakeTime; //上次质押时间
+        uint256 lastRewardCalculationTime; //上次结算奖励时间
+        uint256 rewardsClaimedSoFar; //历史奖励
+    }
+
+    uint256 _minimunStakingAmount;
+    uint256 _maxStakeTokenLimit;
+    uint256 _stakeEndDate;
+    uint256 _stakeStartDate;
+    uint256 _totalStakedTokens;
+
+    uint256 _totalUsers;
+    uint256 _stakeDays;
+    uint256 _earlyUnstakeFeePercentage;
+
+    bool _isStakingPaused;
+    address private _tokenAddress;
+
+    uint256 _apyRate;
+
+    uint256 public constant PERCENTAGE_DENOMINATOR = 10000;
+    uint256 public constant APY_RATE_CHANGE_THRESHOLD = 10;
+
+    mapping(address => User) private _users;
+
+    event Stake(address indexed user, uint256 amount);
+    event UnStake(address indexed user, uint256 amount);
+    event EarlyUnStakeFee(address indexed user, uint256 amount);
+    event ClaimReward(address indexed user, uint256 amount);
+
+    modifier whenTreasuryHasBalance() {
+        require(
+            IERC20(_tokenAddress).balanceOf(address(this)) >= amount,
+            "Treasury has insufficient balance"
+        );
+        _;
+    }
+
+    function initialize(
+        address owner_,
+        address tokenAddress_,
+        uint256 apyRate_,
+        uint256 minimunStakingAmount_,
+        uint256 maxStakeTokenLimit_,
+        uint256 stakeStartDate_,
+        uint256 stakeEndDate_,
+        uint256 stakeDays_,
+        uint256 earlyUnstakeFeePercentage_
+    ) public virtual initializer {
+        __TokenStaking_init_unchained(
+            owner_,
+            tokenAddress_,
+            apyRate_,
+            minimunStakingAmount_,
+            maxStakeTokenLimit_,
+            stakeStartDate_,
+            stakeEndDate_,
+            stakeDays_,
+            earlyUnstakeFeePercentage_
+        );
+    }
+
+    function __TokenStaking_init_unchained(
+        address owner_,
+        address tokenAddress_,
+        uint256 apyRate_,
+        uint256 minimunStakingAmount_,
+        uint256 maxStakeTokenLimit_,
+        uint256 stakeStartDate_,
+        uint256 stakeEndDate_,
+        uint256 stakeDays_,
+        uint256 earlyUnstakeFeePercentage_
+    ) internal onlyInitializing {
+        require(_apyRate <= 10000, "APY rate cannot be more than 100%");
+        require(stakeDays_ > 0, "Stake days cannot be 0");
+        require(tokenAddress_ != address(0), "Token address cannot be 0");
+        require(
+            stakeStartDate_ < stakeEndDate_,
+            "Stake start date cannot be less than end date"
+        );
+        _transferOwnership(owner_);
+        _tokenAddress = tokenAddress_;
+        _apyRate = apyRate_;
+        _minimunStakingAmount = minimunStakingAmount_;
+        _maxStakeTokenLimit = maxStakeTokenLimit_;
+        _stakeStartDate = stakeStartDate_;
+        _stakeEndDate = stakeEndDate_;
+        _stakeDays = stakeDays_ * 1 days;
+        _earlyUnstakeFeePercentage = earlyUnstakeFeePercentage_;
+    }
+}
